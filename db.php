8d650eefb41fad96d5f9c2c9e5924f625a5c7587
Natthachai57624 <111569193+Natthachai57624@users.noreply.github.com>
2022-08-24 17:38:28 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,26 @@
+<?php
+$servername = "remotemysql.com";
+$username = "VeQrto3CUM";
+$password = "z7ddZzT72c";
+$dbname = "VeQrto3CUM";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$conn->set_charset('utf8');
+$sql = "SELECT * from student ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
